a4721717c520f0cae762ec54ad452ed1e1e63748
Healy218 <mrhealy115@gmail.com>
2026-04-24 09:20:38 -0500

    added Exam2

---
+++ Exam2/exam2.R
@@ -0,0 +1,29 @@
+###===== Matt Healy 3824713 =====####
+###=========== Exam 2 ===========####
+###========= 04/24/2026 =========####
+###========= STAT 3612 ==========####
+
+
+# 1) There are two datasets called winequality-red and winequality-white.  These two variants are related to red and white
+# variants of the Portuguese "Vinho Verde" wine.  Your goal is to determine whether the data suggests there is a difference 
+# in PH between the red and white varities. 
+wineRed <- read.csv("winequality-red.csv", header = TRUE, sep = ";")
+wineWhite <- read.csv("winequality-white.csv", header = TRUE, sep = ";")
+
+head(wineRed)
+head(wineWhite)
+
+
+
+
+
+
+
+
+
+houseData <- read.csv("house_data.csv", header = TRUE, sep = ",")
+
+
+
+
+ChickWeight < - read.csv("ChickWeight.csv", header = TRUE, sep = ",")
